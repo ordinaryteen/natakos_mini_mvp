c6725ecc796c5cf82ba4cfb4940398e3fe722b0a
nafiMulyo <muhamadnafimulyo@gmail.com>
2026-02-08 15:54:07 +0700

    making containers

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,77 +12,46 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.white,
 
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+
         children: [
           _searchField(),
+          SizedBox(height: 36,),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Text(
+                  'Category',
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              SizedBox(height: 12,),
+
+              Container(
+                height: 150,
+                color: Colors.green,
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Container();
+                  }
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
   }
 
-  Container _searchField() {
-    return Container(
-        margin: EdgeInsets.only(top: 40, left: 20, right: 20),
 
-        decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                  color: Color(0xff1D1617).withValues(alpha: 0.08),
-                  blurRadius: 40,
-                  spreadRadius: 0.0
-              )
-            ]
-        ),
-
-        child: TextField(
-          style: TextStyle(
-            color: Colors.black45,
-          ),
-          decoration: InputDecoration(
-            hintText: "Masukin sayang...",
-            hintStyle: TextStyle(
-              color: Colors.grey.withValues(alpha:0.6),
-              fontSize: 14,
-            ),
-
-            filled: true,
-            fillColor: Colors.white,
-
-            contentPadding: EdgeInsets.all(15),
-
-            prefixIcon: Padding(
-              padding: const EdgeInsets.all(14),
-              child: SvgPicture.asset('assets/icons/Search.svg'),
-            ),
-            suffixIcon: Container(
-              width: 100,
-              child: IntrinsicHeight(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    VerticalDivider(
-                      color: Colors.black,
-                      indent: 5,
-                      endIndent: 5,
-                      thickness: 0.1,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8, right: 16, bottom: 8, left: 8),
-                      child: SvgPicture.asset('assets/icons/Filter.svg'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: BorderSide.none
-            ),
-          ),
-        )
-    );
-  }
 
   AppBar appBar() {
     return AppBar(
@@ -136,6 +105,72 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+
+  Container _searchField() {
+    return Container(
+        margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+
+        decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xff1D1617).withValues(alpha: 0.06),
+                blurRadius: 20,
+                spreadRadius: 0.0,
+                offset: Offset(0, 8),
+              )
+            ]
+        ),
+
+        child: TextField(
+          style: TextStyle(
+            color: Colors.black45,
+          ),
+          decoration: InputDecoration(
+            hintText: "Masukin sayang...",
+            hintStyle: TextStyle(
+              color: Colors.grey.withValues(alpha:0.6),
+              fontSize: 14,
+            ),
+
+            filled: true,
+            fillColor: Colors.white,
+
+            contentPadding: EdgeInsets.all(15),
+
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(14),
+              child: SvgPicture.asset('assets/icons/Search.svg'),
+            ),
+            suffixIcon: Container(
+              width: 100,
+              child: IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    VerticalDivider(
+                      color: Colors.black,
+                      indent: 5,
+                      endIndent: 5,
+                      thickness: 0.1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, right: 16, bottom: 8, left: 8),
+                      child: SvgPicture.asset('assets/icons/Filter.svg'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none
+            ),
+          ),
+        )
     );
   }
 }
