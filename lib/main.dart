189c649fd43119b2eba9e0942670e1cb3ab501e7
nafiMulyo <muhamadnafimulyo@gmail.com>
2026-02-08 10:52:25 +0700

    initial start: blank screen and remove 'debug' flag

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:natakos_mini_mvp/pages/home.dart';
 
 void main() {
   runApp(const NatakosApp());
@@ -9,49 +10,9 @@ class NatakosApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Natakos Ledger'),
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: const TransactionList(), // Widget utama kita
-      ),
-    );
-  }
-}
-
-class TransactionList extends StatelessWidget {
-  const TransactionList({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // Dummy data berdasarkan COA Natakos
-    final List<Map<String, String>> dummyTransactions = [
-      {'code': '1111', 'name': 'Kas di Tangan Juragan', 'amount': '+ Rp 5.000.000'},
-      {'code': '4112', 'name': 'Sewa Kamar Bulanan', 'amount': '+ Rp 1.500.000'},
-      {'code': '1111', 'name': 'Kas di Tangan Juragan', 'amount': '- Rp 200.000 (Listrik)'},
-    ];
-
-    return ListView.builder(
-      itemCount: dummyTransactions.length,
-      itemBuilder: (context, index) {
-        return Card(
-          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.blue[100],
-              child: Text(dummyTransactions[index]['code']!.substring(0, 2)),
-            ),
-            title: Text(dummyTransactions[index]['name']!),
-            trailing: Text(
-              dummyTransactions[index]['amount']!,
-              style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
-            ),
-          ),
-        );
-      },
+      home: HomePage()
     );
   }
 }
